572ea5563464cd386afdcff1f5ce89c2ecb2189e
Saviskar <Saviskar>
2025-05-16 14:36:40 +0530

    Completed itemdao

--- src/main/com.syos/repository/Impl/ItemDAOImpl.java
+++ src/main/com.syos/repository/Impl/ItemDAOImpl.java
@@ -1,18 +1,24 @@
 package main.com.syos.repository.Impl;
 
+import main.com.syos.model.Batch;
 import main.com.syos.model.Item;
 import main.com.syos.repository.ItemDAO;
 import main.com.syos.util.db.DBConnection;
 
+import java.math.BigDecimal;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class ItemDAOImpl implements ItemDAO {
 
     @Override
     public void createItem(Item item) throws SQLException {
+
         String sql = "INSERT INTO item (item_code, name, unit_price, reorder_level)";
 
         try (Connection conn = DBConnection.getConnection();
@@ -32,27 +38,107 @@ public class ItemDAOImpl implements ItemDAO {
 
     @Override
     public Item findByCode(String code) throws SQLException {
+
+        String sql = "SELECT * FROM item WHERE item_code = ?";
+
+        try (Connection conn = DBConnection.getConnection();
+                PreparedStatement ps = conn.prepareStatement(sql)) {
+
+            ps.setString(1, code);
+
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+                return new Item(
+                        rs.getString("item_code"),
+                        rs.getString("name"),
+                        rs.getBigDecimal("unit_price"),
+                        rs.getInt("reorder_level")
+                );
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         return null;
     }
 
     @Override
     public List<Item> findAll() throws SQLException {
-        return null;
+
+        String sql = "SELECT * FROM item";
+        List<Item> items = new ArrayList<>();
+
+        try (Connection conn = DBConnection.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql);
+             ResultSet rs = ps.executeQuery()) {
+
+            while (rs.next()) {
+                Item item = new Item(
+                        rs.getString("item_code"),
+                        rs.getString("name"),
+                        rs.getBigDecimal("unit_price"),
+                        rs.getInt("reorder_level")
+                );
+                items.add(item);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return items;
     }
+
 
     @Override
     public void updatePrice(String code, double newPrice) throws SQLException {
+        String sql = "UPDATE item SET unit_price = ? WHERE item_code = ?";
 
+        try (Connection conn = DBConnection.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+
+            ps.setBigDecimal(1, BigDecimal.valueOf(newPrice));
+            ps.setString(2, code);
+
+            ps.executeUpdate();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void updateReorderLevel(String code, int newLevel) throws SQLException {
+        String sql = "UPDATE item SET reorder_level = ? WHERE item_code = ?";
 
+        try (Connection conn = DBConnection.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+
+            ps.setInt(1, newLevel);
+            ps.setString(2, code);
+
+            ps.executeUpdate();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void deleteItem(String code) throws SQLException {
+        String sql = "DELETE FROM item WHERE item_code = ?";
 
+        try (Connection conn = DBConnection.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+
+            ps.setString(1, code);
+            ps.executeUpdate();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
